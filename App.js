f2aa439fc0dcaed80deb27904002553818c458cd
ajjubhai786 <74403049+ajjubhai786@users.noreply.github.com>
2022-02-04 18:14:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Nav from './Nav';   
+
+
+function App() {
+  return (
+    <div className='app'>
+
+    <Nav/>
+      <div className="app_body">
+  
+{}
+
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ NavContents.js
@@ -0,0 +1,22 @@
+import  React from 'react';
+import './NavContents.css';
+import {Avatar} from '@material-ui/core';
+
+
+function NavContents({Icon , title,avatar}) {
+
+return <div className= 'navContents_container '>
+
+
+    {Icon && <Icon className='navContents_icon'/>}
+    {avatar && <Avatar/>}
+
+<h3>{title} </h3>
+    
+</div>
+}
+
+
+
+
+export default NavContents;
